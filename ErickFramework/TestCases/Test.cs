ad5504c5521ca3dc498f40b90930a5d3af428164
Erick Unosquare <erick.jimenez@unosquare.com>
2021-03-13 21:55:08 -0600

    My first commit

---
+++ ErickFramework/TestCases/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace TestCases
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Console.WriteLine("erick jimenez");
+        }
+    }
+}
