9f64f3c7b5f35ea35ad221410144b19fccaaa1cf
erick9025 <erick9025@hotmail.com>
2021-10-24 19:29:50 -0500

    Made it work again

--- ErickFramework/Framework/Extras/SampleClass.cs
+++ ErickFramework/Framework/Extras/SampleClass.cs
@@ -1,6 +1,8 @@
-﻿using System;
-using System.Threading;
+﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+//using OpenQA.Selenium.Chrome;
 
 namespace Framework
 {
@@ -16,7 +18,7 @@ namespace Framework
 
             driver.Manage().Window.Maximize();
             driver.Navigate().GoToUrl("https://google.com");
-            driver.FindElementByName("q").SendKeys("Erick Eduardo Jimenez Rodriguez");
+            driver.FindElement(By.Name("q")).SendKeys("Erick Eduardo Jiménez Rodríguez");
 
             Thread.Sleep(5000);
 
